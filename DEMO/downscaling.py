cd0990963e7c1802058158ad5035bfc0e0c9bda2
Snowystar122 <llb.2001@live.com>
2023-05-02 12:33:06 +0100

    Added files

---
+++ DEMO/downscaling.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import os
+
+# Load the image
+im_repo = "instance_10"
+
+for im in os.listdir(im_repo):
+    image = Image.open(f"{im_repo}/" + f"{im}")
+    downscaled_image = image.resize((650, 650))
+    downscaled_image.save(f"{im_repo}/" + f"{im}")
+
